dda1ff43e1d33d0528160dd8550cbb8c705cd06c
BitterB0NG0 <bitterestb0ng0@gmail.com>
2022-02-01 15:49:42 -0700

    revert to avoid fixing merge

--- RobotContainer.java
+++ RobotContainer.java
@@ -7,8 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.ManualDriveCommand;
-import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -19,20 +17,14 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  /** The robot's subsystems and commands are defined here...
-   * Establishing Instances of Subsystem, Command, & RobotContainer Classes */
-  private final ManualDriveCommand manualDriveCommand = new ManualDriveCommand();
-
-  public final static DriveSubsystem driveSubsystem = new DriveSubsystem();
-
-
+  // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    /** Configure the button bindings 
-     * 
-    */
+    // Configure the button bindings
     configureButtonBindings();
   }
 
