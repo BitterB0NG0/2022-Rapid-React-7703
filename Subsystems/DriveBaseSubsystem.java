6597bdd5dacd36e82f051a6b6bffe55f52c049da
BjornTheProgrammer <75190918+BjornTheProgrammer@users.noreply.github.com>
2022-03-16 13:20:00 -0700

    Update DriveBaseSubsystem.java

--- Subsystems/DriveBaseSubsystem.java
+++ Subsystems/DriveBaseSubsystem.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveBaseSubsystem extends SubsystemBase {
+    boolean movePosition = false;
 
     public static WPI_VictorSPX frontLeftDriveMotorController = new WPI_VictorSPX(Constants.frontLeftDriveMotorControllerPort);
     public static WPI_VictorSPX backLeftDriveMotorController = new WPI_VictorSPX(Constants.backLeftDriveMotorControllerPort);
@@ -23,11 +24,23 @@ public class DriveBaseSubsystem extends SubsystemBase {
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+        if (movePosition = true) {
+            moveDistance(50, Constants.distance);
+        }
     }
 
     @Override
     public void simulationPeriodic() {
         // This method will be called once per scheduler run during simulation
+    }
+
+    // position should be in centimeters
+    public void moveDistance(double finalPosition, double currentPosition) {
+        if (finalPosition > currentPosition) {
+            leftDriveMotorControllerGroup.set(0.0004 * (finalPosition - currentPosition) * (finalPosition - currentPosition));
+        } else {
+            leftDriveMotorControllerGroup.set(-0.0004 * (finalPosition - currentPosition) * (finalPosition - currentPosition));
+        }
     }
 
     public void drivePercentArcade(double xSpeed, double zRotation, boolean squareInputs) {
