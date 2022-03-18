0c61a1563deaeff63f835a487025b0ccf064eef7
BitterB0NG0 <bitterestb0ng0@gmail.com>
2022-03-18 08:16:01 -0700

    Updating Constants.java

--- Constants.java
+++ Constants.java
@@ -39,4 +39,8 @@ public final class Constants {
     public static double quadinglePower = .3;
     public static double floppaPower;
     public static double bingusPower;
+
+    public static double shooterAngle = 1.309; // Angle in Radians (75 Degrees)
+    public static double gravitationalFieldStrenth = 9.80665; // Gravitational Field Stregnth in Meters per Second per Second
+    public static double maximumHeight = 8; // Maximym Height in Meters
 }
